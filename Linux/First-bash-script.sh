676707c610f3da79fc7b28f8eb9dc27b8abedde0
Alex <alexcastro@Alexs-MacBook-Pro.local>
2021-05-01 21:52:31 -0700

    First commit

---
+++ Linux/First-bash-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir ~/research 2> /dev/null
+
+echo "A Quick System Audit Script" >  ~/research/sys_info.txt
+date >> ~/research/sys_info.txt
+echo "" >> ~/research/sys_info.txt
+echo "Machine Type Info:" >> ~/research/sys_info.txt
+echo $MACHTYPE >> ~/research/sys_info.txt
+echo -e "Uname info: $(uname -a) \n" >> ~/research/sys_info.txt
+echo -e "IP Info: $(ip addr | grep inet | tail -2 | head -1) \n" >> ~/research/sys_info.txt
+echo "Hostname: $(hostname -s) " >> ~/research/sys_info.txt
+
+
---
+++ Linux/for-loop.sh
@@ -0,0 +1,11 @@
+for state in ${states[@]}
+do
+
+  if [ $state == 'Hawaii' ];
+  then
+    echo "Hawaii is the best!"
+  else
+    echo "I'm not a fan of Hawaii."
+  fi
+done
+
